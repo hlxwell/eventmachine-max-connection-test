ba4420803549f9dcdb4798eb10d200ef9bd20d56
Michael He <hlxwell@gmail.com>
2012-03-22 11:33:49 +0800

    add node js server

--- client.rb
+++ client.rb
@@ -1,23 +1,18 @@
 require "bundler/setup"
 require 'eventmachine'
 
-# $conn_num = 0
-
 module ClientHandler
   def post_init
-    # puts "Starting connect #{$conn_num += 1}"
   end
 
   def unbind
-    # puts "unbinded -------- ${$conn_num -= 1}"
   end
 end
 
-new_size = EM.set_descriptor_table_size( 60000 )
-puts "=== file descriptor size: #{new_size}"
-
 EM.epoll if EM.epoll?
 EM.kqueue = true if EM.kqueue?
+
+puts "=== file descriptor size: #{EM.set_descriptor_table_size}"
 
 EM.run do
   EM.add_periodic_timer(0.001) do
--- server.rb
+++ server.rb
@@ -1,25 +1,19 @@
 require "bundler/setup"
 require 'eventmachine'
 
-$conn_num = 0
-
 class ServerHandler < EventMachine::Connection
   def post_init
-    $conn_num += 1
-    puts "+++ Total #{$conn_num} clients connected"
+    puts "+++ Total #{EM.connection_count} clients connected"
   end
 
   def unbind
-    # $conn_num -= 1
-    # puts "--- unbinded -------- #{$conn_num}"
   end
 end
 
-new_size = EM.set_descriptor_table_size( 60000 )
-puts "=== file descriptor size: #{new_size}"
-
 EM.epoll if EM.epoll?
 EM.kqueue = true if EM.kqueue?
+
+puts "=== file descriptor size: #{EM.set_descriptor_table_size}"
 
 EM.run do
   EM::start_server "0.0.0.0", 9999, ServerHandler
